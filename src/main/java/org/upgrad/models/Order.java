31dd61913303e3a793757c8ca3b0f36bae7239e3
swaroopchirravuri <swaroopchirravuri@gmail.com>
2018-11-30 13:38:29 +0530

    implementation of getCouponByCouponName endpoint with 3/4 test pass

--- src/main/java/org/upgrad/models/Order.java
+++ src/main/java/org/upgrad/models/Order.java
@@ -1,4 +1,135 @@
 package org.upgrad.models;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
+import javax.persistence.*;
+import java.util.Date;
+
+/*
+ * Order model class contain all the attributes to be mapped to all the fields in the orders table in the database.
+ * Annotations are used to specify all the constraints to the table and table-columns in the database.
+ * Here getter, setter and constructor are defined for this model class.
+ */
+
+
+@Entity
+@Table(name = "ORDERS")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "bill",nullable = false)
+    private double bill;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Coupon coupon;
+
+    @Column(name = "discount")
+    private double discount;
+
+    @Column(name = "date")
+    private Date date = new Date();
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    //@ManyToOne
+    //@OnDelete(action = OnDeleteAction.CASCADE)
+    //private Payment payment;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Address address;
+
+    public Order(){}
+
+    public Order(String categoryName, double bill, Coupon coupon, double discount, Date date, User user, /*Payment payment,*/ Address address) {
+        this.categoryName = categoryName;
+        this.bill = bill;
+        this.coupon = coupon;
+        this.discount = discount;
+        this.date = date;
+        this.user = user;
+        //this.payment = payment;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public double getBill() {
+        return bill;
+    }
+
+    public void setBill(double bill) {
+        this.bill = bill;
+    }
+
+    public Coupon getCoupon() {
+        return coupon;
+    }
+
+    public void setCoupon(Coupon coupon) {
+        this.coupon = coupon;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /*public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+*/
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
--- src/main/java/org/upgrad/services/OrderService.java
+++ src/main/java/org/upgrad/services/OrderService.java
@@ -1,5 +1,9 @@
 package org.upgrad.services;
 
+import org.upgrad.models.Coupon;
+
 public interface OrderService {
 
+    //Get coupon details by name.
+    Coupon getCoupon(String couponName);
 }
