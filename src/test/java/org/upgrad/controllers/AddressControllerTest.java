5504cb300df080bd404e557b4764f1017cad5b0a
sugandha <sugs.sehgal2006@gmail.com>
2018-12-03 13:03:42 +0530

    minor changes

--- src/test/java/org/upgrad/controllers/AddressControllerTest.java
+++ src/test/java/org/upgrad/controllers/AddressControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.upgrad.models.States;
+import org.upgrad.models.States;
 import org.upgrad.models.UserAuthToken;
 import org.upgrad.services.AddressService;
 import org.upgrad.services.UserAuthTokenService;
