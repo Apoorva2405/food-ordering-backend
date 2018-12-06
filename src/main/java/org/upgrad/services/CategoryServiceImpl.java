9f40fb5da513bcd2e1ecd65a88d3fe1edff32c21
swaroopchirravuri <swaroopchirravuri@gmail.com>
2018-12-06 22:06:36 +0530

    fix for Sweet Dish issue

--- src/main/java/org/upgrad/services/CategoryServiceImpl.java
+++ src/main/java/org/upgrad/services/CategoryServiceImpl.java
@@ -27,8 +27,23 @@ public class CategoryServiceImpl implements CategoryService{
 
         // Case insenstive Check
         String s1 = categoryName.substring(0, 1).toUpperCase();
-        String nameCapitalized = s1 + categoryName.substring(1).toLowerCase();
+        String s2 = "null";
+        String nameCapitalized = "null";
+        int index = 0;
+        if (categoryName.contains(" ")){
+            index = categoryName.indexOf(" ");
+            s2 = categoryName.substring(index+1,index+2).toUpperCase();
+        }
 
+        if (s2.equalsIgnoreCase("null")) {
+
+             nameCapitalized = s1 + categoryName.substring(1).toLowerCase();
+        }
+        else {
+
+             nameCapitalized = s1 + categoryName.substring(1,index+1).toLowerCase() + s2 + categoryName.substring(index+2).toLowerCase();
+        }
+        
         return categoryRepository.getCategoryByName(nameCapitalized);
 }
 
